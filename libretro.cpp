a9af30b7732494939e2afda06d048dac42e15e06
twinaphex <libretro@gmail.com>
2014-06-23 03:58:07 +0200

    Sleep function can go

--- libretro.cpp
+++ libretro.cpp
@@ -7,7 +7,6 @@
 #include	"mednafen/video/Deinterlacer.h"
 #endif
 #include "libretro.h"
-#include "thread.h"
 
 static MDFNGI *game;
 
@@ -4178,9 +4177,4 @@ void MDFND_PrintError(const char* err)
 {
    if (log_cb)
       log_cb(RETRO_LOG_ERROR, "%s\n", err);
-}
-
-void MDFND_Sleep(unsigned int time)
-{
-   retro_sleep(time);
 }
